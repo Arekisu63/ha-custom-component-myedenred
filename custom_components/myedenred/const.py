f4bc2f124317f0a71529fde30b1dea03119afaa5
Arekisu63 <131964628+Arekisu63@users.noreply.github.com>
2023-11-01 20:35:15 +0100

    Update const.py
    Change URL .fr

--- custom_components/myedenred/const.py
+++ custom_components/myedenred/const.py
@@ -2,7 +2,7 @@ DOMAIN = "myedenred"
 PLATFORM = "sensor"
 DOMAIN_DATA = f"{DOMAIN}_data"
 
-ATTRIBUTION = "Data provided by https://www.myedenred.pt/"
+ATTRIBUTION = "Data provided by https://www.myedenred.fr/"
 
 DEFAULT_ICON = "mdi:credit-card"
 UNIT_OF_MEASUREMENT = "€"
